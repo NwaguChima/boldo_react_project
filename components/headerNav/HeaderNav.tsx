8cee53d791c1254624833c36b56fcde64afedcc1
NwaguChima <nwaguchima48@gmail.com>
2022-09-02 13:15:48 +0100

    completed mockup for headernav

--- components/headerNav/HeaderNav.tsx
+++ components/headerNav/HeaderNav.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface HeaderNavProps {}
@@ -7,6 +8,24 @@ const HeaderNav: React.FC<HeaderNavProps> = ({}) => {
   return (
     <nav>
       <Image src="/images/logo.svg" alt="logo" width={100} height={42} />
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Product</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Services</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>Log In</li>
+      </ul>
     </nav>
   );
 };
