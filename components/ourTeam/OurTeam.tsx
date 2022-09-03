e141d2914ce1767f0e576430042b5bab6e2f873a
NwaguChima <nwaguchima48@gmail.com>
2022-09-04 00:43:47 +0100

    setup jsx for our team

--- components/ourTeam/OurTeam.tsx
+++ components/ourTeam/OurTeam.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import AboutHeading2 from "../aboutHeading2/AboutHeading2";
 import styles from "./OurTeam.module.scss";
@@ -12,6 +13,23 @@ export const OurTeam: React.FC<OurTeamProps> = ({}) => {
         heading="The leadership team"
         text2="Conversion angel investor entrepreneur first mover advantage. Handshake infographic mass market crowdfunding iteration. Traction stock user experience deployment beta innovator incubator focus. "
       />
+      <div className="container">
+        <div className="card">
+          <img src="/images/team-1.svg" alt="Michael Scott" />
+          <h4>Michael Scott</h4>
+          <p>General Manager</p>
+        </div>
+        <div className="card">
+          <img src="/images/team-2.svg" alt="Dwight Schrute" />
+          <h4>Dwight Schrute</h4>
+          <p>General Manager</p>
+        </div>
+        <div className="card">
+          <img src="/images/team-3.svg" alt="Jim Halpert" />
+          <h4>Jim Halpert</h4>
+          <p>General Manager</p>
+        </div>
+      </div>
     </section>
   );
 };
