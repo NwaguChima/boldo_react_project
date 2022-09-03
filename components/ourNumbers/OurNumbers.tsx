d83a44ddeb6ee972fa682edcd4960e2868c9091f
NwaguChima <nwaguchima48@gmail.com>
2022-09-04 00:13:10 +0100

    styled numbers

--- components/ourNumbers/OurNumbers.tsx
+++ components/ourNumbers/OurNumbers.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import AboutHeading2 from "../aboutHeading2/AboutHeading2";
-import sytles from "./OurNumbers.module.scss";
+import styles from "./OurNumbers.module.scss";
 
 interface OurNumbersProps {}
 
 export const OurNumbers: React.FC<OurNumbersProps> = ({}) => {
   return (
-    <section className={sytles.numbers}>
+    <section className={styles.numbers}>
       <AboutHeading2
         text="Our numbers"
         heading="Handshake infographic mass market crowdfunding iteration."
         center
         mode="dark"
       />
+      <div className={styles.figures}>
+        <div className={styles.figures__item}>
+          <span>120m</span>
+          <p>Cool feature title</p>
+        </div>
+        <div className={styles.figures__item}>
+          <span>10.000</span>
+          <p>Cool feature title</p>
+        </div>
+        <div className={styles.figures__item}>
+          <span>240</span>
+          <p>Cool feature title</p>
+        </div>
+      </div>
     </section>
   );
 };
