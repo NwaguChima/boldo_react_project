b9bb4f0a0dad74cd2ae258eee33d905b67b60541
NwaguChima <nwaguchima48@gmail.com>
2022-09-03 12:00:18 +0100

    added images

--- components/ourStory/OurStory.tsx
+++ components/ourStory/OurStory.tsx
@@ -1,9 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import styles from "./OurStory.module.scss";
 
 interface OurStoryProps {}
 
 const OurStory: React.FC<OurStoryProps> = ({}) => {
-  return <div>Hello</div>;
+  return (
+    <section>
+      <div className={styles.ourStory}>
+        <img src="/images/story-1.svg" alt="Our Story" />
+        <img src="/images/story-2.svg" alt="Our Story" />
+        <img src="/images/story-3.svg" alt="Our Story" />
+        <img src="/images/story-4.svg" alt="Our Story" />
+        <img src="/images/story-5.svg" alt="Our Story" />
+      </div>
+    </section>
+  );
 };
 
 export default OurStory;
