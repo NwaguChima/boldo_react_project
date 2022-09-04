7f2721b8a7fa4554aee41285e22bdf11dc7ab138
NwaguChima <nwaguchima48@gmail.com>
2022-09-04 02:09:55 +0100

    setup footer

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./Footer.module.scss";
+
+interface FooterProps {}
+
+const Footer: React.FC<FooterProps> = ({}) => {
+  return <div>Foootere</div>;
+};
+
+export default Footer;
