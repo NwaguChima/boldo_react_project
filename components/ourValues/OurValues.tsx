52beb92ba39be5d55de9d1096c58f826f472c5c0
NwaguChima <nwaguchima48@gmail.com>
2022-09-04 01:36:13 +0100

    added basic styles to values

--- components/ourValues/OurValues.tsx
+++ components/ourValues/OurValues.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import AboutHeading2 from "../aboutHeading2/AboutHeading2";
 import styles from "./OurValues.module.scss";
@@ -13,6 +14,53 @@ const OurValues: React.FC<OurValuesProps> = ({}) => {
         text2="Conversion angel investor entrepreneur first mover advantage. Handshake infographic mass market crowdfunding iteration. Traction stock user experience deployment beta innovator incubator focus. "
         mode="dark"
       />
+      <div className={styles.values__container}>
+        <div className={styles.values__container__item}>
+          <Image
+            src="/images/values-1.svg"
+            alt="We believe in"
+            height={150}
+            width={150}
+          />
+          <div className={styles.value__text}>
+            <h4>We are commited.</h4>
+            <p>
+              Conversion angel investor entrepreneur first mover advantage.
+              Handshake infographic mass market crowdfunding iteration.{" "}
+            </p>
+          </div>
+        </div>
+        <div className={styles.values__container__item}>
+          <Image
+            src="/images/values-2.svg"
+            alt="We believe in"
+            height={150}
+            width={150}
+          />
+          <div className={styles.value__text}>
+            <h4>We are responsible.</h4>
+            <p>
+              Conversion angel investor entrepreneur first mover advantage.
+              Handshake infographic mass market crowdfunding iteration.
+            </p>
+          </div>
+        </div>
+        <div className={styles.values__container__item}>
+          <Image
+            src="/images/values-3.svg"
+            alt="We believe in"
+            height={150}
+            width={150}
+          />
+          <div className={styles.value__text}>
+            <h4>We aim for progress.</h4>
+            <p>
+              Conversion angel investor entrepreneur first mover advantage.
+              Handshake infographic mass market crowdfunding iteration.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
