50da3fb5c76f49125066269bbe7c8ceffa09cdd6
NwaguChima <nwaguchima48@gmail.com>
2022-09-04 01:24:35 +0100

    added heading

--- components/ourValues/OurValues.tsx
+++ components/ourValues/OurValues.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import AboutHeading2 from "../aboutHeading2/AboutHeading2";
+import styles from "./OurValues.module.scss";
 
 interface OurValuesProps {}
 
 const OurValues: React.FC<OurValuesProps> = ({}) => {
-  return <div>Value</div>;
+  return (
+    <section className={styles.values}>
+      <AboutHeading2
+        text="Our values"
+        heading="Things in we believe"
+        text2="Conversion angel investor entrepreneur first mover advantage. Handshake infographic mass market crowdfunding iteration. Traction stock user experience deployment beta innovator incubator focus. "
+        mode="dark"
+      />
+    </section>
+  );
 };
 
 export default OurValues;
