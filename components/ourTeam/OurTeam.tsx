63497fbfd96ffe4477caaaac536cd5268b539f3a
NwaguChima <nwaguchima48@gmail.com>
2022-09-04 01:06:22 +0100

    completed our team

--- components/ourTeam/OurTeam.tsx
+++ components/ourTeam/OurTeam.tsx
@@ -13,18 +13,18 @@ export const OurTeam: React.FC<OurTeamProps> = ({}) => {
         heading="The leadership team"
         text2="Conversion angel investor entrepreneur first mover advantage. Handshake infographic mass market crowdfunding iteration. Traction stock user experience deployment beta innovator incubator focus. "
       />
-      <div className="container">
-        <div className="card">
+      <div className={styles.container}>
+        <div className={styles.container__card}>
           <img src="/images/team-1.svg" alt="Michael Scott" />
           <h4>Michael Scott</h4>
           <p>General Manager</p>
         </div>
-        <div className="card">
+        <div className={styles.container__card}>
           <img src="/images/team-2.svg" alt="Dwight Schrute" />
           <h4>Dwight Schrute</h4>
           <p>General Manager</p>
         </div>
-        <div className="card">
+        <div className={styles.container__card}>
           <img src="/images/team-3.svg" alt="Jim Halpert" />
           <h4>Jim Halpert</h4>
           <p>General Manager</p>
