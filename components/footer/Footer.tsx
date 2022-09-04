545ba234c87b9ec6dda7fc979bc47eb5f22d0c6c
NwaguChima <nwaguchima48@gmail.com>
2022-09-04 02:36:52 +0100

    styled input

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -8,10 +8,10 @@ const Footer: React.FC<FooterProps> = ({}) => {
     <footer className={styles.footer}>
       <div className={styles.footer__box}>
         <h2>An enterprise template to ramp up your company website</h2>
-        <div>
-          <input type="text" placeholder="Your email address" />
+        <form className={styles.footer__box__form}>
+          <input type="email" placeholder="Your email address" />
           <button>Subscribe</button>
-        </div>
+        </form>
       </div>
     </footer>
   );
