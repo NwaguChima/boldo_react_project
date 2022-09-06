cb2189d3b18bf19bf07158155b60b1f00a8f84da
NwaguChima <nwaguchima48@gmail.com>
2022-09-06 16:56:56 +0100

    setup dummy data

---
+++ utils/data.js
@@ -0,0 +1,52 @@
+export const blog = [
+  {
+    title: "Category",
+    text: "Pitch termsheet backing validation focus release.",
+    date: "November 22, 2021",
+    image: "/images/grid-1.svg",
+    author: "Chandler Bing",
+    authorBadge: "/images/grid-1-1.svg",
+  },
+  {
+    title: "Category",
+    text: "Seed round direct mailing non-disclosure agreement graphical user interface rockstar.",
+    date: "November 22, 2021",
+    image: "/images/grid-2.svg",
+    author: "Rachel Green",
+    authorBadge: "/images/grid-2-1.svg",
+  },
+  {
+    title: "Category",
+    text: "Beta prototype sales iPad gen-z marketing network effects value proposition",
+    date: "November 22, 2021",
+    image: "/images/grid-3.svg",
+    author: "Monica Geller",
+    authorBadge: "/images/grid-3-1.svg",
+  },
+
+  {
+    title: "Category",
+    text: "Pitch termsheet backing validation focus release.",
+    date: "November 22, 2021",
+    image: "/images/grid-4.svg",
+    author: "Chandler Bing",
+    authorBadge: "/images/grid-1-1.svg",
+  },
+  {
+    title: "Category",
+    text: "Seed round direct mailing non-disclosure agreement graphical user interface rockstar.",
+    date: "November 22, 2021",
+    image: "/images/grid-5svg",
+    author: "Rachel Green",
+    authorBadge: "/images/grid-2-1.svg",
+  },
+
+  {
+    title: "Category",
+    text: "Beta prototype sales iPad gen-z marketing network effects value proposition",
+    date: "November 22, 2021",
+    image: "/images/grid-6.svg",
+    author: "Monica Geller",
+    authorBadge: "/images/grid-3-1.svg",
+  },
+];
