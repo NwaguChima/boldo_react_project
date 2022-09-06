4fbead57d38c849f9c0d2c0f8bccf8809a6f8580
NwaguChima <nwaguchima48@gmail.com>
2022-09-06 15:08:53 +0100

    setup our blog

---
+++ components/ourBlog/OurBlog.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./OurBlog.module.scss";
+
+interface OurBlogProps {}
+
+const OurBlog: React.FC<OurBlogProps> = ({}) => {
+  return <div>Imeela</div>;
+};
+export default OurBlog;
